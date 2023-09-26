fa7bbf51ccfaf1f2c6ca353ec6742943e7af8a2a
Robert Kroeger <rjkroege@liqui.org>
2023-09-26 11:10:06 -0400

    Parse and truncate the prospective tags

--- Sources/command_line.swift
+++ Sources/command_line.swift
@@ -9,17 +9,29 @@ struct Tag: ParsableCommand {
   @Flag(name: .shortAndLong, help: "clear the tags set on the file first or append them")
   var reset = false
 
-	// TODO(rjk): Parse the tags out. They have leading '@' signs.
+  // TODO(rjk): Parse the tags out. They have leading '@' signs.
   @Argument(help: "Tags and files")
   var filesAndTags: [String]
 
   // I don't mutate this structure. If I assigned to self?
   mutating func run() throws {
+    // Why can't I just use the standard integer subscripting? Is this some
+    // kind of protecting me from wrong unicode habits thing? For low-level
+    // String thwacking, Go or Zig would seem to be a better fit.
+    let tags = filesAndTags.filter({ s in s.hasPrefix("@") }).map({ s in
+      String.init(
+        s.suffix(from: s.index(s.startIndex, offsetBy: 1)))
+    })
+    let files = filesAndTags.filter({ s in !s.hasPrefix("@") })
 
-	// TODO(rjk): Sort out the tags
-
-    for t in filesAndTags {
-      printFinderTags(t)
+    if tags.count > 0 {
+      for f in files {
+        setFinderTags(tags, f, reset)
+      }
+    } else {
+      for t in files {
+        printFinderTags(t)
+      }
     }
   }
 }
