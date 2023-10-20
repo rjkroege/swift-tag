d7594d491209476fb22deacc2f2311c0dd32a98e
Robert Kroeger <rjkroege@liqui.org>
2023-10-19 20:05:04 -0400

    Correctly sets tags on a file with none

--- Sources/set_tags.swift
+++ Sources/set_tags.swift
@@ -1,26 +1,37 @@
 import Foundation
 
+// TODO(rjk): reset doesn't seem to be useful?
 func setFinderTags(_ tags: [String], _ fn: String, _ reset: Bool) {
+  // Makes a url corresponding to a specific file path.
   let url = URL.init(fileURLWithPath: fn)
 
+  // Tries to retreive the tagNames resource for this URL. This has three
+  // outcomes: success, nil (meaning that there are no tagNames and throws
+  // for an I/O error or some such.
   let urvs = try? url.resourceValues(forKeys: [
     URLResourceKey.tagNamesKey
   ])
+
+  // Give up if there was some kind of I/O error
   guard let r_urvs = urvs else {
-    print("setFinderTags resourceValues failed!")
+    print("setFinderTags resourceValues failed. \(fn)  probably doesn't exist")
     return
   }
 
-  // Make a set
-  guard let r_tagnames = r_urvs.tagNames else {
-    print("getting tagNames failed")
-    return
+  // The file existed and had resources. If the tagNames are nil, it means
+  // that everything is good except that there aren't any tagNames set on
+  // this file.
+  if let r_tagnames = r_urvs.tagNames {
+    // Merge the old and new tags.
+    let tagset = Set(r_tagnames)
+    let mergedtagset = tagset.union(tags).map { $0 }
+    setHelper(url, mergedtagset)
+  } else {
+    setHelper(url, tags)
   }
+}
 
-  // Merge the set with tags
-  let tagset = Set(r_tagnames)
-  let mergedtagset = tagset.union(tags).map { $0 }
-
+func setHelper(_ url: URL, _ mergedtagset: [String]) {
   // There is a bug in Foundation maybe? See [Setting filesystem metadata
   // from S… | Apple Developer
   // Forums](https://developer.apple.com/forums/thread/667379)
